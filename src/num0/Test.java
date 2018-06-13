732b236cc7096eaf3a110175eda43fe3bc886089
JYB <JYB@JYB-PC>
2018-06-13 21:58:58 +0800

    工作描述

--- src/num0/Test.java
+++ src/num0/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("ÎÒ½ĞÇØÏş¿µ");
+		System.out.println("123456789");
+		System.out.println("2345678910");
 	}
-
 }
